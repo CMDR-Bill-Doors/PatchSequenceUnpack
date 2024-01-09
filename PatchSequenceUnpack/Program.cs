0ac7176d7aedaddbf6033dd0be161c1273e5e694
CMDR-Bill-Doors <Billyanghk@qq.com>
2024-01-09 22:05:19 +0000

    make loadfolders and logs

--- PatchSequenceUnpack/Program.cs
+++ PatchSequenceUnpack/Program.cs
@@ -5,12 +5,15 @@ using System.Collections.Generic;
 using System.Reflection;
 using static System.Net.Mime.MediaTypeNames;
 using System.Text;
+using System.Linq;
 
 namespace PatchSequenceUnpack
 {
     internal class Program
     {
         static XmlWriter result;
+
+        static TextWriter exportLog;
 
         static string dir = System.IO.Directory.GetCurrentDirectory();
 
@@ -20,6 +23,10 @@ namespace PatchSequenceUnpack
 
         static int j;
 
+        static bool makeLoadFolders;
+
+        static List<string> firstFolders = new List<string>();
+
         static void Main(string[] args)
         {
             Console.WriteLine("Input the patch folder's path");
@@ -28,22 +35,45 @@ namespace PatchSequenceUnpack
             Console.WriteLine("Input the output folder's path");
             outputDir = Path.Combine(Console.ReadLine(), "Output");
 
+            Console.WriteLine("Create pesudo loadfolder file? Y/N");
+            makeLoadFolders = Console.ReadLine().ToLower()[0] == 'y';
+            if (makeLoadFolders)
+            {
+                Console.WriteLine("A pesudo loadfolder file will be created");
+            }
+
             DirectoryInfo directoryInfo = new DirectoryInfo(dir);
             Console.WriteLine("Output " + outputDir);
             Directory.CreateDirectory(outputDir);
+
+            exportLog = File.AppendText(Path.Combine(outputDir, "log.txt"));
 
             foreach (FileInfo file in directoryInfo.GetFiles("*.xml", SearchOption.AllDirectories))
             {
                 Read(file);
             }
-            Console.WriteLine($"Converted patches stored at {outputDir}");
-            Console.WriteLine($"De-Sequencing of {i} files and {j} patches Complete, Press any key to exit.");
+
+            if (makeLoadFolders)
+            {
+                GenLoadFolders();
+            }
+
+
+            Log($"Converted patches stored at {outputDir}");
+            Log($"De-Sequencing of {i} files and {j} patches Complete, Press any key to exit.");
+            exportLog.Close();
             Console.ReadKey();
+        }
+
+        public static void Log(string logMessage)
+        {
+            Console.WriteLine(logMessage);
+            exportLog.WriteLine(logMessage);
         }
 
         static void Read(FileInfo file)
         {
-            Console.WriteLine("Reading " + file.Name);
+            Log("Reading " + file.FullName);
             try
             {
                 XmlDocument xmlDoc;
@@ -61,7 +91,7 @@ namespace PatchSequenceUnpack
 
                 if (!IsPatch(xmlDoc))
                 {
-                    Console.WriteLine(file.FullName + " is not a patch");
+                    Log(file.FullName + " is not a patch");
                     return;
                 }
                 XmlWriterSettings writterSetting = new XmlWriterSettings();
@@ -74,10 +104,17 @@ namespace PatchSequenceUnpack
                 s.Remove(0, 1);
                 string filepath = s.ToString();
                 string firstFolder = filepath.Split(@"\".ToCharArray())[0];
+
+                if (!firstFolders.Contains(firstFolder))
+                {
+                    firstFolders.Add(firstFolder);
+                }
+
                 s.Replace(file.Name, "");
                 string directirory = s.ToString();
                 Directory.CreateDirectory(Path.Combine(outputDir, firstFolder, "Patches", directirory));
                 result = XmlWriter.Create(Path.Combine(outputDir, firstFolder, "Patches", filepath), writterSetting);
+                Log("Writing to " + Path.Combine(outputDir, firstFolder, "Patches", filepath));
                 result.WriteStartElement("Patch");
                 UnPackPatch(xmlDoc);
                 i++;
@@ -87,7 +124,7 @@ namespace PatchSequenceUnpack
             }
             catch (Exception ex)
             {
-                Console.WriteLine("Exception reading " + file.Name + ": " + ex);
+                Log("Exception reading " + file.Name + ": " + ex);
             }
         }
 
@@ -161,7 +198,6 @@ namespace PatchSequenceUnpack
                 {
                     if (childNode.Name == "operations")
                     {
-                        Console.WriteLine("unpacking sequence");
                         UnPackSequenceInner(childNode);
                     }
                 }
@@ -173,12 +209,49 @@ namespace PatchSequenceUnpack
             foreach (XmlNode childNode in sequence.ChildNodes)
             {
                 XmlNode newNode = childNode.OwnerDocument.CreateElement("Operation");
-                foreach (XmlAttribute att in childNode.Attributes) newNode.Attributes.Append(att);
+
+                XmlAttribute[] attributes = new XmlAttribute[childNode.Attributes.Count];
+                childNode.Attributes.CopyTo(attributes, 0);
+
+                foreach (XmlAttribute attribute in attributes)
+                {
+                    newNode.Attributes.Append(attribute);
+                }
+
                 newNode.InnerXml = childNode.InnerXml;
 
                 newNode.WriteTo(result);
                 j++;
             }
+        }
+
+        static void GenLoadFolders()
+        {
+            XmlWriterSettings writterSetting = new XmlWriterSettings();
+            writterSetting.Indent = true;
+            writterSetting.IndentChars = "\t";
+            Directory.CreateDirectory(outputDir);
+            result = XmlWriter.Create(Path.Combine(outputDir, "LoadFolders.xml"), writterSetting);
+            result.WriteStartElement("loadFolders");
+
+            result.WriteComment("This file is a pesudo-LoadFolders, auto created by the unpacker.\n You will need to replace ifModActives to corresponding packageID, and replace REPLACEMENT-CTRL-Hs to some other path like 1.4/mods/ to work.");
+
+            result.WriteStartElement("v1.4");
+
+            result.WriteStartElement("li");
+            result.WriteString("/");
+            result.WriteEndElement();
+
+            foreach (string s in firstFolders)
+            {
+                result.WriteStartElement("li");
+                result.WriteAttributeString("IfModActive", s);
+                result.WriteString("REPLACEMENT-CTRL-H/" + s);
+                result.WriteEndElement();
+            }
+
+            result.WriteEndElement();
+            result.Close();
         }
     }
 }
